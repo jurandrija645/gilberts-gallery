b57901952361eab955f75d926ec55b83c7fc7d87
Andrija Juran <jurandrija645@gmail.com>
2025-01-25 12:29:55 +0100

    changing index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <p>Gallery loading...</p>
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      Gallery is being built...
+    </main>
   );
 }
