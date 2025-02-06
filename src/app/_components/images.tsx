c55510e5009530e19e6d8066820bb3135faa3a04
Andrija Juran <jurandrija645@gmail.com>
2025-02-06 20:39:22 +0100

    Adding image upload and adding images to the database

--- src/app/_components/images.tsx
+++ src/app/_components/images.tsx
@@ -7,8 +7,8 @@ interface ImagesProps {
 export function Images({ images }: ImagesProps) {
   return (
     <div className="flex flex-wrap gap-4">
-      {[...images, ...images, ...images, ...images].map((image, index) => (
-        <div key={`${image.id}-${index}`} className="flex w-48 flex-col">
+      {[...images].map((image) => (
+        <div key={image.id} className="flex w-48 flex-col">
           <img src={image.url} alt="image" />
           <div>{image.name}</div>
         </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,13 @@
 import "~/styles/globals.css";
+import "@uploadthing/react/styles.css";
+
 import { ClerkProvider } from "@clerk/nextjs";
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 import TopNav from "./_components/topnav";
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+import { extractRouterConfig } from "uploadthing/server";
+import { ourFileRouter } from "./api/uploadthing/core";
 
 export const metadata: Metadata = {
   title: "Gilberts Gallery",
@@ -15,6 +20,15 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <ClerkProvider>
+      <NextSSRPlugin
+        /**
+         * The `extractRouterConfig` will extract **only** the route configs
+         * from the router to prevent additional information from being
+         * leaked to the client. The data passed to the client is the same
+         * as if you were to fetch `/api/uploadthing` directly.
+         */
+        routerConfig={extractRouterConfig(ourFileRouter)}
+      />
       <html lang="en" className={`${GeistSans.variable}`}>
         <body className={`flex flex-col gap-4 font-sans text-white`}>
           <TopNav />
