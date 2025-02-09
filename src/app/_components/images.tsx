d1b49f76676d6041016e3e1c85e9370a8942bf65
Andrija Juran <jurandrija645@gmail.com>
2025-02-09 19:24:12 +0100

    Adding next image

--- src/app/_components/images.tsx
+++ src/app/_components/images.tsx
@@ -1,4 +1,5 @@
 import { images } from "../../server/db/schema";
+import Image from "next/image";
 
 interface ImagesProps {
   images: (typeof images.$inferSelect)[];
@@ -8,8 +9,14 @@ export function Images({ images }: ImagesProps) {
   return (
     <div className="flex flex-wrap gap-4">
       {[...images].map((image) => (
-        <div key={image.id} className="flex w-48 flex-col">
-          <img src={image.url} alt="image" />
+        <div key={image.id} className="flex h-48 w-48 flex-col">
+          <Image
+            src={image.url}
+            style={{ objectFit: "contain" }}
+            width={192}
+            height={192}
+            alt={image.name}
+          />
           <div>{image.name}</div>
         </div>
       ))}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,16 +9,16 @@ export default async function HomePage() {
 
   return (
     <main>
-      <div className="mx-auto flex w-[80%] flex-col items-center justify-center rounded-lg bg-gray-900 p-8">
+      <div className="mx-auto flex w-[80%] flex-col items-center justify-center gap-8 rounded-lg bg-gray-900 p-8">
         <SignedOut>
           <p className="h-full w-full text-center text-2xl">Please sign in</p>
         </SignedOut>
         <SignedIn>
           <Images images={images} />
+          <button className="mb-4 mt-10 rounded-lg bg-blue-500 px-4 py-2 font-semibold text-white transition-colors hover:bg-blue-600">
+            Upload Image
+          </button>
         </SignedIn>
-        <button className="mt-4 rounded-lg bg-blue-500 px-4 py-2 font-semibold text-white transition-colors hover:bg-blue-600">
-          Upload Image
-        </button>
       </div>
     </main>
   );
