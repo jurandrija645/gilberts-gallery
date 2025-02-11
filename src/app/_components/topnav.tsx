5cd4a1d772a6d4d2a5c48b1031f3b5089255612e
Andrija Juran <jurandrija645@gmail.com>
2025-02-11 23:46:18 +0100

    Update topnav.tsx

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -7,7 +7,7 @@ import { useRouter } from "next/navigation";
 export default function TopNav() {
   const router = useRouter();
   return (
-    <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
+    <nav className="flex w-full items-center justify-between border-b px-10 py-4 text-xl font-semibold">
       <div>Gallery</div>
       <div className="flex flex-row gap-4">
         <SignedOut>
