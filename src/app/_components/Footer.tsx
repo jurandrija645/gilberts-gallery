ca8472caaada88289c176f9d5887fab1aa86fa76
Andrija Juran <jurandrija645@gmail.com>
2025-02-14 21:06:44 +0100

    Update Footer.tsx

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -3,7 +3,8 @@ export function Footer() {
     <footer className="mt-auto border-t py-4">
       <div className="container mx-auto px-4">
         <p className="text-center text-sm text-gray-600">
-          © {new Date().getFullYear()} Gilberts Gallery. All rights reserved.
+          © {new Date().getFullYear()} Gilberts Gallery. All rights reserved -
+          Valmedia LLC.
         </p>
       </div>
     </footer>
